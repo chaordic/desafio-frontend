65e089d959037251d3e48978b0947321854b4314
yuri mello <contato@yurimello.com>
2020-11-08 15:59:57 -0300

    inserindo suporte a sass no projeto

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import '../style/style.scss';
 
 const Index = () => {
   return (
     <Layout>
-      <div>
+      <div className="test">
           <h1>Home</h1>
           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic 
             eaque explicabo inventore cum totam? Eligendi iure a odit, fugit reiciendis natus voluptate dolorum aliquid vel veniam quos, cupiditate, aperiam dignissimos?</p>
