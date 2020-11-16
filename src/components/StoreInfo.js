f5864185e2d88a879118c22a45c0f2b8dd21e72b
ymello <contato@yurimello.com>
2020-11-16 19:14:41 -0300

    consumindo dados datail

--- src/components/StoreInfo.js
+++ src/components/StoreInfo.js
@@ -12,26 +12,32 @@ const StoreInfo = ({breweries,location}) => {
     useGetSingleData(id)
   }
 
-  const { name } = single
-  return (
-    <Col xs={12} sm={12} md={12}>
-      <div className="card-no-hover">
-          <div className="title-box">
-              <h1 className="title">{name}</h1>
-          </div>
-          <div className="card-box">
-              <span>Type: Regional</span>
-              <span>Street: 3301 Madison Rd</span>
-              <span>City: Cincinnati</span>
-              <span>State: Ohio</span>
-              <span>Country: United States</span>
-              <span>Website: http://www.site.com.br</span>
-              <span>Phone: 2138368733</span>
-              <span>Open in Maps: 39.1563456, 39.1563456</span>
-          </div>
-      </div>
-    </Col>
-  )
+  if(single) {
+    const { name, brewery_type, street, city, state, country, website_url, phone, latitude, longitude } = single
+    console.log(single)
+
+    return (
+      <Col xs={12} sm={12} md={12}>
+        <div className="card-no-hover">
+            <div className="title-box">
+                <h1 className="title">{name}</h1>
+            </div>
+            <div className="card-box">
+                <span>Type: {brewery_type} </span>
+                <span>Street: {street}</span>
+                <span>City: {city}</span>
+                <span>State: {state}</span>
+                <span>Country: {country}</span>
+                <span>Website: {website_url}</span>
+                <span>Phone: {phone}</span>
+                <span>Open in Maps: {`${latitude},${longitude}`}</span>
+            </div>
+        </div>
+      </Col>
+    )
+  }
+
+  return null
 };
 
 export default connect(state => state)(StoreInfo);
