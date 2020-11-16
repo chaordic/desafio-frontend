20023e14491751ea51eee8ba01f8e5bc461ced22
ymello <contato@yurimello.com>
2020-11-16 18:14:57 -0300

    criando chamada apenas 1 elemento

--- src/stores/brewary/action.js
+++ src/stores/brewary/action.js
@@ -3,6 +3,7 @@ import {BREWERIES_URL} from '../../assets/constants';
 
 export const brewariesActionTypes = {
   SUCESS: 'BREWERY_LIST_SUCESS',
+  SINGLE_SUCESS: 'BREWERY_SINGLE_SUCESS',
 }
 
 export const getBreweries = () => (dispatch) => {
@@ -10,6 +11,17 @@ export const getBreweries = () => (dispatch) => {
     const data = response.data;
     return dispatch({
       type: brewariesActionTypes.SUCESS,
+      data,
+    })
+  })
+}
+
+
+export const getBrewerie = (id) => (dispatch) => {
+  axios.get(`${BREWERIES_URL}/${id}`).then(response => {
+    const data = response.data;
+    return dispatch({
+      type: brewariesActionTypes.SINGLE_SUCESS,
       data,
     })
   })
--- src/stores/brewary/reducer.js
+++ src/stores/brewary/reducer.js
@@ -1,14 +1,21 @@
 import {brewariesActionTypes} from './action';
 
 const initialState = {
-  data: []
+  data: [],
+  single: {}
 }
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case brewariesActionTypes.SUCESS:
       return Object.assign({}, state, {
+        ...state,
         data: action.data
+      })
+    case brewariesActionTypes.SINGLE_SUCESS:
+      return Object.assign({}, state, {
+        ...state,
+        single: action.data
       })
     default:
       return state
