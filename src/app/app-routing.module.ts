8cb7fe5c1434a5a832326d42219026b6a9fb03af
Lucas Barros Rodrigues <lucas.barros.dev@gmail.com>
2021-03-17 23:15:52 -0300

    mais ajustes de layout na listagem + navegacao para detalhes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrewerieDetailsComponent } from './pages/brewerie-details/brewerie-details.component';
 import { BreweriesListComponent } from './pages/breweries-list/breweries-list.component';
 
 
 const routes: Routes = [
-  { path: '', component: BreweriesListComponent }
+  { path: '', component: BreweriesListComponent },
+  { path: 'details', component: BrewerieDetailsComponent }
 ];
 
 @NgModule({
--- src/app/pages/breweries-list/breweries-list.component.ts
+++ src/app/pages/breweries-list/breweries-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'lnx-breweries-list',
@@ -7,11 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreweriesListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   temp = [1,2,3,4,5,6,7,8];
 
   ngOnInit(): void {
+  }
+
+  navigateToDetails(){
+    this.router.navigate(['details']);
   }
 
 }
