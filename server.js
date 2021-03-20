09471f3145d3379ce6061909f1ded8a01387a2e2
Lucas Barros Rodrigues <lucas.barros.dev@gmail.com>
2021-03-19 22:41:00 -0300

    fixes pre-deploy

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/dist/desafio-brewery-linx'));
+
+app.get('/*', (req,res) =>{
+    res.sendFile(__dirname + 'dist/desafio-brewery-linx/index.html');
+})
+
+app.listen(PORT, () =>{
+    console.log('servidor iniciado');
+})
