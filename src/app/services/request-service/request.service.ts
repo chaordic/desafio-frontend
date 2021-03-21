66e647cbca9965e577f86f4606a14e65f62b8674
Lucas Barros Rodrigues <lucas.barros.dev@gmail.com>
2021-03-21 14:54:38 -0300

    limpeza de código + melhorias de performance

--- src/app/services/request-service/request.service.ts
+++ src/app/services/request-service/request.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { Observable } from 'rxjs';
 
@@ -13,11 +13,17 @@ export class RequestService {
   constructor(private http: HttpClient) { }
 
   doGet(endpoint: string) : Observable<any> {
-    return this.http.get(this.api + endpoint);
+    return this.http.get(this.api + endpoint, this.getHeader());
   }
 
   doPost(endpoint: string, object: Object) : Observable<any> {
-    return this.http.post(this.api + endpoint, object);
+    return this.http.post(this.api + endpoint, object, this.getHeader());
+  }
+
+  getHeader(): any{
+    const headers = new HttpHeaders()
+    .set('Accept-Encoding', 'gzip, compress, br')
+    return headers;
   }
 
 }
