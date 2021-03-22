f3b86dae88987775d5f0e814809eefc676a39c48
Lucas Barros Rodrigues <lucas.barros.dev@gmail.com>
2021-03-21 21:35:11 -0300

    server.js consertado

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const compression = require('compression');
 
 const PORT = process.env.PORT || 8080;
 
-//app.use(express.static(__dirname + '/dist/desafio-brewery-linx'));
+app.use(express.static(__dirname + '/dist/desafio-brewery-linx'));
 
 app.use(compression());
 
